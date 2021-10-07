a257c851120c0619536dbfa30d8ce1116f2eccdb
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-07 17:09:00 +0530

    update: add url to collection

--- lib/widgets/meta_info.dart
+++ lib/widgets/meta_info.dart
@@ -4,6 +4,7 @@ import 'package:todo_supabase/providers/meta_collection_provider.dart';
 import 'package:todo_supabase/providers/meta_info_provider.dart';
 import 'package:todo_supabase/screens/webview_url.dart';
 import 'package:todo_supabase/services/meta_info_service.dart';
+import 'package:todo_supabase/utils/constants.dart';
 import 'package:todo_supabase/widgets/create_new_collection.dart';
 import 'package:todo_supabase/widgets/rounded_button.dart';
 
@@ -127,7 +128,8 @@ FutureBuilder<dynamic> MetaInfoDisplay(
                               buttonText: "Save URL",
                               width: MediaQuery.of(context).size.width * 0.8,
                               onpressed: () {
-                                viewUserCollection(context, _controller);
+                                addURLToCollection(context, _controller,
+                                    photoURL, title, type, url);
                               },
                             ),
                           ],
@@ -150,9 +152,9 @@ FutureBuilder<dynamic> MetaInfoDisplay(
   );
 }
 
-Future<dynamic> viewUserCollection(
-    BuildContext context, TextEditingController _controller) {
-  return showModalBottomSheet(
+addURLToCollection(BuildContext context, TextEditingController _controller,
+    String photoURL, String title, String type, MetaInfoProvider url) async {
+  showModalBottomSheet(
     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
     context: context,
     shape: RoundedRectangleBorder(
@@ -165,7 +167,7 @@ Future<dynamic> viewUserCollection(
           return Column(
             children: [
               SizedBox(
-                height: 15,
+                height: 25,
               ),
               RoundedButtonWidget(
                 buttonText: "Create new collection",
@@ -173,6 +175,9 @@ Future<dynamic> viewUserCollection(
                 onpressed: () {
                   createNewCollection(context, _controller);
                 },
+              ),
+              SizedBox(
+                height: 25,
               ),
               userMetaCollection.map(
                 data: (res) {
@@ -184,10 +189,118 @@ Future<dynamic> viewUserCollection(
                       itemBuilder: (BuildContext context, int index) {
                         return Column(
                           children: [
-                            Text(
-                              res.value.data[index]['collection_name'],
-                              style: TextStyle(color: Colors.white),
+                            WhiteButton(
+                              buttonText: res.value.data[index]
+                                  ['collection_name'],
+                              width: MediaQuery.of(context).size.width * 0.9,
+                              onpressed: () async {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      backgroundColor:
+                                          Theme.of(context).accentColor,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(15.0),
+                                        ),
+                                      ),
+                                      title: Text(
+                                        "Save to ${res.value.data[index]['collection_name']}",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      content: Text(
+                                        "Are you sure!?",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      buttonPadding: EdgeInsets.all(
+                                        10,
+                                      ),
+                                      actions: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(15.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            children: [
+                                              ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Text("Cancel"),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: Theme.of(context)
+                                                      .errorColor,
+                                                  padding: EdgeInsets.symmetric(
+                                                    horizontal: 25,
+                                                    vertical: 15,
+                                                  ),
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                      10,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              ElevatedButton(
+                                                onPressed: () async {
+                                                  await supabase
+                                                      .from("URLCollection")
+                                                      .insert([
+                                                    {
+                                                      'created_by': supabase
+                                                          .auth
+                                                          .currentUser
+                                                          ?.email,
+                                                      'photoURL':
+                                                          photoURL.toString(),
+                                                      'title': title.toString(),
+                                                      'type': type.toString(),
+                                                      'url': url.url,
+                                                    }
+                                                  ]).execute();
+                                                  Navigator.popUntil(
+                                                    context,
+                                                    (route) => route.isFirst,
+                                                  );
+                                                  // showSnackBar(context,
+                                                  //     "Added to ${fetchedURLCollection['names'][i]['name']} collection!");
+                                                },
+                                                child: Text("OK"),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: Theme.of(context)
+                                                      .primaryColor,
+                                                  padding: EdgeInsets.symmetric(
+                                                    horizontal: 35,
+                                                    vertical: 15,
+                                                  ),
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                      10,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
                             ),
+                            SizedBox(height: 15),
                           ],
                         );
                       },
