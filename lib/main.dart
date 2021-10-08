40f58e00dfd87283eec32cd1fe49f7717eb2f6df
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-08 21:51:18 +0530

    Revert "update: alarm manager initialized"
    This reverts commit ad16e5cc7f8b58958e4e385d7659658e668f4967.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -15,7 +14,6 @@ import 'package:todo_supabase/utils/theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await AndroidAlarmManager.initialize();
   Supabase.initialize(
     url: dotenv.env["SUPABASE_URL"],
     anonKey: dotenv.env["SUPABASE_ANON_KEY"],
